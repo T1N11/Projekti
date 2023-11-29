dd86b7b0aef9b256aeb62affa401f5e4889037d1
GentZE04 <gent3543@gmail.com>
2023-11-29 13:20:42 +0100

    account.js

--- js/account.js
+++ js/account.js
@@ -6,3 +6,31 @@ function resnav() {
         x.className = "topnav";
     }
 }
+function validateReg() {
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value; 
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+
+    var nameRegex = /^[a-zA-z\s]+$/;
+    if(!nameRegex.test(username)) {
+        alert('Please enter a valid name!');
+        return false;
+    }
+
+    var emailRegex = /^[a-zA-z.-_]+@+[a-z]+\.+[a-z]{2,3}$/;
+    if(!emailRegex.test(email)) {
+        alert("Please enter a valid email address!");
+        return false;
+    }
+
+    if(password.length < 6) {
+        alert('Password must be at least 6 characters!');
+        return false;
+    }
+
+    if (confirmPassword !== password) {
+        alert('Passwords do not match');
+        return false;
+    }
+}
